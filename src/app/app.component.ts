624af9ba5bf5d54b2c97f4450843069732fcee00
Christopher Rescorla <cgrescorla@gmail.com>
2017-02-06 10:01:48 -0700

    why didn't these go?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFire, FirebaseListObservable} from "angularfire2";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = "Lets build an RPG!!";
+    characters: FirebaseListObservable<any[]>;
+    constructor(af: AngularFire) {
+        this.characters = af.database.list("/characters");
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AngularFireModule} from "angularfire2";
+import {MaterialModule} from "@angular/material";
+import "hammerjs";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCaFpifiBHt8a7JDRNcyQOlWtFExswm_4k",
+    authDomain: "my-test-rpg.firebaseapp.com",
+    databaseURL: "https://my-test-rpg.firebaseio.com",
+    storageBucket: "my-test-rpg.appspot.com",
+    messagingSenderId: "713128869102"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidemenuComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        MaterialModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
